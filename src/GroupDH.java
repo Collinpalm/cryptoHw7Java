71a140a8222753f3e17921c5f2e747833d964530
Collinpalm <collinpalm@gmail.com>
2021-12-03 21:30:55 -0500

    finished the adduser method

--- src/GroupDH.java
+++ src/GroupDH.java
@@ -8,6 +8,8 @@ import java.io.*;
 
 public class GroupDH {
     public static KeyNode root;
+    long p;
+    long g;
 
     public static void main(String[] args)throws FileNotFoundException, IOException{
         File file=new File(args[0]);
@@ -15,16 +17,19 @@ public class GroupDH {
         BufferedReader br=new BufferedReader(fr);
         String line = br.readLine();
         String[] splitLine = line.split(" ");
-        BigInteger p = new BigInteger(splitLine[0]);
-        BigInteger g = new BigInteger(splitLine[1]);
+        p = Long.parseLong(splitLine[0]);
+        g = Long.parseLong(splitLine[1]);
         int count = Integer.parseInt(br.readLine());
         for(int i = 0;i<count;i++){
             line = br.readLine();
             splitLine = line.split(" ");
             if(i == 0){
-                root = new KeyNode(null, splitLine[4]);
-                root.leftUser = new User(root, Integer.parseInt(splitLine[1]), splitLine[0]);
-                root.rightUser = new User(root, Integer.parseInt(splitLine[3]), splitLine[2]);
+                root = new KeyNode(null, null, null, 0, splitLine[4]);
+                root.leftKid = new KeyNode(root, null, null, Integer.parseInt(splitLine[1]), splitLine[0]);
+                root.rightKid = new KeyNode(root, null, null, Integer.parseInt(splitLine[3]), splitLine[2]);
+                int a = root.leftKid.key;
+                int b = root.rightKid.key;
+                root.key = (int)((Math.pow(g, (a*b)))%p);
             }else if(splitLine[0] == "ADD"){
                 addUser(splitLine[1], Integer.parseInt(splitLine[2]), splitLine[3], Integer.parseInt(splitLine[4]), splitLine[5]);
             }else if(splitLine[0] == "DEL"){
@@ -37,13 +42,20 @@ public class GroupDH {
 
     }
     public static void addUser(String sponsorName, int newKey1, String newUserName, int newKey2, String keyID){
-
+        KeyNode sponsor = recurseKeyNode(sponsorName, root);
+        sponsor.key = newKey1;
+        KeyNode newShare = new KeyNode(sponsor.parent, sponsor, null,0, keyID );
+        sponsor.parent = newShare;
+        newShare.rightKid = new KeyNode(newShare, null, null, newKey2, newUserName);
+        int a = newShare.leftKid.key;
+        int b = newShare.rightKid.key;
+        root.key = (int)((Math.pow(g, (a*b)))%p);
     }
     public static void deleteUser(String SponsorName, int newKey){
 
     }
     public static void query(String searchName){
-        System.out.println(recurseKeyNode(searchName, root).name);
+        System.out.println(recurseKeyNode(searchName, root).key);
     }
     private static KeyNode recurseKeyNode(String target, KeyNode pos){
         if(pos.name == target){
@@ -63,23 +75,14 @@ public class GroupDH {
 //this is the shared key nodes class
 class KeyNode{
     public KeyNode parent, leftKid, rightKid;
-    public User leftUser, rightUser;
     public String name;
-    public KeyNode(KeyNode inputParent, String inputName){
-        parent = inputParent;
-        name = inputName;
-    }
-}
-
-//this is the actual user node class
-class User{
-    public KeyNode parent;
     public int key;
-    public String name;
-
-    public User(KeyNode inputparent, int inputkey, String inputName){
-        parent = inputparent;
+    public KeyNode(KeyNode inputParent, KeyNode left, KeyNode right, int inputkey, String inputName){
+        parent = inputParent;
+        leftKid = left;
+        rightKid = right;
         key = inputkey;
         name = inputName;
     }
 }
+
